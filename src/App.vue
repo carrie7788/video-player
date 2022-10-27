b1f3c31e7961f7ee94674be3f1d7db6bea12119a
zhaodandan <zhaodandan@kedacom.com>
2022-10-27 14:51:18 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -24,17 +24,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
-#wrap {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  box-sizing: border-box;
-  .video-box {
-    position: absolute;
-    width: 100%;
-    height: 80%;
-  }
-}
+// #wrap {
+//   width: 100%;
+//   height: 100%;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   box-sizing: border-box;
+//   .video-box {
+//     position: absolute;
+//     width: 100%;
+//     height: 100%;
+//   }
+// }
 </style>
